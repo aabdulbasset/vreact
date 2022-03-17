8efc3088c774488d980172c3c702e3c5a78cbc2f
aabdulbasset5@gmail.com <aabdulbasset5@gmail.com>
2022-03-17 13:23:51 +0200

    I hate git, footer and other optimizations

--- push.sh
+++ push.sh
@@ -5,5 +5,5 @@ git checkout -b  build
 git remote add origin https://github.com/aabdulbasset/vreact.git
 git add .
 git commit -m "Automated push"
-git push origin build
+git push origin build --force
 ssh -p 65002 u123510861@153.92.6.127 "cd public_html && rm -rf asset-manifest.json favicon.ico index.html static temp .git && git clone -b build --single-branch https://github.com/aabdulbasset/vreact.git temp && cd temp && find . -maxdepth 1 -exec mv {} .. \; && cd .. && rm -rf temp"
